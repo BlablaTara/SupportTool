5b2aa4b0b3d7d9a70c3791d9fadb1f5c880d6e2e
BlablaTara <tara0561@stud.kea.dk>
2025-11-18 21:23:07 +0100

    added cors, now backend + frontend talking

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import homeRouter from "./routes/homeRouter.js"
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/", homeRouter);
---
+++ frontend/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from "svelte";
+
+    let message = "Loading...";
+
+    onMount(async () => {
+        try {
+            const res = await fetch ("http://localhost:8080/");
+            message = await res.text();
+
+        } catch (error) {
+            message = "could not reach backend";
+            console.error(error);
+        }
+    });
+</script>
+
+<h1>{message}</h1>
