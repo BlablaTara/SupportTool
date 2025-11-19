4daa8816883443602df3ae38ac715bc29be2e92c
BlablaTara <tara0561@stud.kea.dk>
2025-11-19 15:58:18 +0100

    added a generic section component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+import Section from "./components/sections/Section.svelte";
   import EmailCheck from "./components/EmailCheck.svelte";
   import ResultItem from "./components/ResultItem.svelte";
   import { checks } from "./stores/checksStore.js";
@@ -6,8 +7,18 @@
 
 <h1>Support Tool</h1>
 
-<EmailCheck />
+<div class="section-container">
+    <Section title="User Validation">
+    <EmailCheck />
+        {#if $checks}
+        <ResultItem {...$checks} />
+        {/if}
+    </Section>
 
-{#if $checks}
-    <ResultItem {...$checks} />
-{/if}
+    <Section title="DB Validation">
+    </Section>
+
+    <Section title="BTP Service Validation">
+    </Section>
+</div>
+
---
+++ frontend/src/components/sections/Section.svelte
@@ -0,0 +1,17 @@
+<script>
+  import '../../../public/css/section.css'
+    export let title = "Section Title";
+</script>
+
+<section class="section">
+    <h2 class="section-title">{title}</h2>
+
+    <div class="section-content">
+        <slot></slot>
+    </div>
+
+    <!-- <div class="section-summary">
+    <Summary ... />
+    </div> -->
+
+</section>
