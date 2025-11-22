2401eb5e352575d41dd38f70af4e09ab949c7498
BlablaTara <tara0561@stud.kea.dk>
2025-11-23 00:52:16 +0100

    errorhandling

--- backend/db/couchbase/emailCheck.js
+++ backend/db/couchbase/emailCheck.js
@@ -1,12 +1,12 @@
 import { connectCouchbase, BUCKET, SCOPE } from "./couchbaseDriver.js";
 
 export async function emailCheckCB(collection, email) {
-    const { cluster, bucket } = await connectCouchbase();
+    const { cluster } = await connectCouchbase();
 
-    console.log("Querying Couchbase with:");
-    console.log("Bucket:", BUCKET);
-    console.log("Scope:", SCOPE);
-    console.log("Collection:", collection);
+    // console.log("Querying Couchbase with:");
+    // console.log("Bucket:", BUCKET);
+    // console.log("Scope:", SCOPE);
+    // console.log("Collection:", collection);
 
     const query = `
         SELECT * 
@@ -16,24 +16,32 @@ export async function emailCheckCB(collection, email) {
 
     try {
         const result = await cluster.query(query, { parameters: { email } });
-        console.log("Query result:", result.rows);
+        // console.log("Query result:", result.rows);
         return { ok: true, items: result.rows.map(r => r[collection]) };
 
     } catch (err) {
         // Couchbase missing index = code 4000
         console.error("Couchbase query error:", err);
         const missingIndex = err?.cause?.first_error_code === 4000;
+        
+        const keyspaceNotFound = err?.cause?.first_error_message?.includes("Keyspace not found");
 
         if (missingIndex) {
             return {
                 ok: false,
                 error: "MISSING_INDEX",
-                message: "Required index is missing.",
+                message: "Required index is missing",
                 detail: `CREATE INDEX idx_email ON \`${BUCKET}\`.\`${SCOPE}\`.\`${collection}\`(email);`
+            };
+        } else if (keyspaceNotFound) {
+            return {
+                ok: false,
+                error: "KEYSPACE_NOT_FOUND",
+                message: `The bucket/scope/collection does not exist`,
+                detail: err.cause.first_error_message 
             };
         }
 
-        // General Couchbase failure
         return {
             ok: false,
             error: "QUERY_FAILED",
