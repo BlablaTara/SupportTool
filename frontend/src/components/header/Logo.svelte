5c11b1e2541b107b55b7138cd94475fc149a8d20
BlablaTara <tara0561@stud.kea.dk>
2025-12-10 10:58:19 +0100

    added logo and styled a bit more

--- frontend/src/components/header/Logo.svelte
+++ frontend/src/components/header/Logo.svelte
@@ -1,1 +1,21 @@
-<div class="logo">Support Tool</div>
+
+<script>
+
+</script>
+
+<div class="logo">
+    <img src="/images/Trifork_logo_RGB.png" alt="Support Tool Logo" class="logo-image" />
+</div>
+
+<style>
+.logo {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem; 
+}
+
+.logo-image {
+    height: 40px; 
+    width: auto;
+}
+</style>
